b83d6b7f53a8bc63c56ade194aef22a3df6f4655
Stephane Thomas <thomas.stephn@gmail.com>
2013-06-05 00:37:37 -0700

    Initial Rails Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuoterRails::Application.config.secret_token = '7eeee59d33e8d75e9f65e06d03756fc50a74d8468a3996b42c25907a173da456fa9dfea0182fae7de8704a6c33f10f4104691d86be9fe30181f4fb73cbfae605'
