c431e141539a1efb6c7da32197fc452b41066837
Stephane Thomas <thomas.stephn@gmail.com>
2013-07-01 09:45:57 -0700

    Updated relation model user modified:   app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,10 @@ class User < ActiveRecord::Base
   has_many :quotes_products, through: :quotes
   has_many :products, through: :quotes_products
   has_many :products
+
+  validates :name, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  attr_accessible :name, :first_name, :last_name, :status, :password_digest, :email, :password
+
   
 end
